e52b0387801418e7ade20996b64cd4349bfd8d2b
Slavica <zsmiha@netzero.net>
2015-02-17 01:47:38 -0500

    Answer#10

--- refactor.js
+++ refactor.js
@@ -292,3 +292,37 @@ var addNumbers = function(numberA, numberB) {
 };
 
 //var twoPlusTwo = addNumbers(2,2);
+
+// -----------------------------------------------
+
+//////////////////////////////////////////////////
+//////////////////////////////////////////////////
+
+// 10. -------------------------------------------
+
+// Below is a snippet of code taken from a racing
+// game (not really!) It allows you to accelerate
+// the speed by a given amount. The problem is if
+// you call the function without specifying an
+// amount, it inadvertently sets the speed to NaN
+// First write a comment that explains why it was
+// setting speed to NaN when no parameter is given
+// Then refactor the function to have a default
+// amount of 1 if no param is given.
+
+var speed = 0;
+
+var accelerate = function(amount) {
+  speed += amount;
+};
+
+// Put your answer below -------------------------
+var speed = 0;
+
+var accelerate = function(amount) {
+  if (amount === undefined) {
+      amount = 0;
+  } 
+
+  speed += amount;
+};
