cd2f4e67f531f6a13479102144961e6982c47785
Slavica <zsmiha@netzero.net>
2015-02-17 01:22:52 -0500

    Answer#1

---
+++ refactor.js
@@ -0,0 +1,37 @@
+//////////////////////////////////////////////////
+//////////////////////////////////////////////////
+//     ___  ___________  _______________  ___   //
+//    / _ \/ __/ __/ _ |/ ___/_  __/ __ \/ _ \  //
+//   / , _/ _// _// __ / /__  / / / /_/ / , _/  //
+//  /_/|_/___/_/ /_/ |_\___/ /_/  \____/_/|_|   //
+//                                              //
+// The below assignment has 10 questions with 1 //
+// bonus question. Each question has a specific //
+// area for you to put your answer. That is the //
+// only part that should be changed. Do not     //
+// edit the code in the question. Your answers  //
+// should be valid javascript and any comments  //
+// should be commented out. When finished this  //
+// entire document should be valid javascript.  //
+//                                              //
+//////////////////////////////////////////////////
+//////////////////////////////////////////////////
+
+// 1. --------------------------------------------
+
+// This is a named function, convert it
+// to a variable containing an anonymous
+// function
+
+function doSomethingCool() {
+  console.log("Something Cool!");
+}
+
+// Put your answer below -------------------------
+
+var convertToAnonymous = function() {
+  console.log("Something Cool!");
+}
+
+// call function
+convertToAnonymous();
